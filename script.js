efd3e2d87ad6e175c542944c1d2c261810fa15a6
PayalSingh04 <97962955+PayalSingh04@users.noreply.github.com>
2022-02-04 08:32:35 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".whack1").click(function(){
+    $(".mole1").hide();
+});
+    
+$(".whack2").click(function(){
+    $(".mole-two").hide();
+});
+    
+$(".whack3").click(function(){
+    $(".third-mole").hide();
+});
+    
+$(".resetmoles").click(function(){
+    $(".mole1").show();
+    $(".mole-two").show();
+    $(".third-mole").show();
+});
